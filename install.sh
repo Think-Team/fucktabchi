64a3f6fdd35a0b39d30b1a90fab7e5ba570a71b7
siktirmirza <alicowman79@gmail.com>
2019-08-31 18:45:49 +0430

    --no-check-certificate
     for bypass ssl cert. check of fucking valtman.name's certificate

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget "https://valtman.name/files/telegram-cli-1222"
+wget "https://valtman.name/files/telegram-cli-1222" --no-check-certificate
 sudo apt-get install libreadline-dev
 sudo apt-get install libconfig-dev 
 sudo apt-get install libssl-dev 
@@ -37,5 +37,5 @@ chmod 777 telegram-cli-1222
 RED='\033[0;31m'
 NC='\033[0m'
 CYAN='\033[0;36m'
-echo -e "${CYAN}Installation Completed! Create a bot with creator.py (python3 creator.py)${NC}"
+echo -e "${CYAN}Installation Completed! Create a bot with creator.py (python3 creator.py) :D${NC}"
 exit
